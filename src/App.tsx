e1938b9040aeb6d97105265275e12dd7a65767c8
Philipp Paeßens  (Praktikant) <philipp.paessens@smartcondata.de>
2025-07-09 11:01:19 +0200

    Schöner gemacht

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import { useState } from 'react';
+import Fab from '@mui/material/Fab';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 
 interface nachricht {
   message: string;
@@ -16,37 +21,40 @@ export default function Chat() {
   console.log(speicher)
   return (
     <>
-      <button onClick={() => setUser(1)}>Person 1</button>
-      <button onClick={() => setUser(2)}>Person 2</button>
+      <Fab color="error" variant="extended" onClick={() => user === 1 ? setUser(2) : setUser(1)}>Switch to User {user === 1 ? "2" : "1"}</Fab>
       <h1>Angemeldet: User {user}</h1>
       <div style={{
         height: "300px",
+        width: "500px",
         overflowY: "auto",
         border: "1px solid gray",
         padding: "8px",
         marginTop: "16px",
       }}>
-          <h3>Nachrichten-Chat mit User {user === 1 ? "2" : "1"}</h3>
-          <div style={{
-            height: "220px",
-            overflowY: "auto",
-            border: "1px solid gray",
-            padding: "8px",
-            marginTop: "16px",
-            display: "flex",
-            flexDirection: "column-reverse"
-          }}>
-            {speicher.slice().reverse().map((nachricht) => (
-              <div key={nachricht.id} style={{ textAlign: nachricht.user === user ? "right" : "left", marginBottom: "4px" }}>{nachricht.message}</div>
-            ))}
+        <h3>Nachrichten-Chat mit User {user === 1 ? "2" : "1"}</h3>
+        <div style={{
+          height: "220px",
+          overflowY: "auto",
+          border: "1px solid gray",
+          padding: "8px",
+          marginTop: "16px",
+          display: "flex",
+          flexDirection: "column-reverse"
+        }}>
+          {speicher.slice().reverse().map((nachricht) => (
+            <div key={nachricht.id} style={{ textAlign: nachricht.user === user ? "right" : "left", marginBottom: "4px" }}>{nachricht.message}</div>
+          ))}
         </div>
       </div>
-      <input type="text" placeholder="Nachricht" value={message} onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={() => {
-        setSpeicher([...speicher, { message: message, user: user, id: number }])
-        setNumber(prevNumber => prevNumber - 1)
-        setMessage("")
-      }}>Send</button>
+      <br />
+      <Box display="flex" gap="10px">
+        <TextField sx={{ width: '450px' }} id="outlined-basic" variant="outlined" type="text" placeholder="Nachricht" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Fab color="success" onClick={() => {
+          setSpeicher([...speicher, { message: message, user: user, id: number }])
+          setNumber(prevNumber => prevNumber - 1)
+          setMessage("")
+        }}><SendIcon /></Fab>
+      </Box>
     </>
   );
 }
